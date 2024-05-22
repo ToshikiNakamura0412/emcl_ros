7576f0a64c6f414b49c974f78e9d09be5179a306
Toshiki Nakamura <tnakamura.amsl@gmail.com>
2024-05-23 00:16:45 +0900

    Add comments

--- include/utils/pose.h
+++ include/utils/pose.h
@@ -11,20 +11,73 @@
 #include <cmath>
 #include <utility>
 
+/**
+ * @class Pose
+ * @brief Class of Pose
+ */
 class Pose
 {
 public:
-  Pose();
+  /**
+   * @brief Construct a new Pose object
+   */
+  Pose(void);
+
+  /**
+   * @brief Construct a new Pose object
+   * @param x x-coordinate
+   * @param y y-coordinate
+   * @param yaw yaw
+   */
   Pose(const float x, const float y, const float yaw);
+
+  /**
+   * @brief Copy constructor
+   * @param pose Pose object
+   */
   Pose &operator=(const Pose &pose);
+
+  /**
+   * @brief Division operator
+   * @param a float
+   */
   Pose &operator/=(const float a);
 
+  /**
+   * @brief Set the pose
+   * @param x x-coordinate
+   * @param y y-coordinate
+   * @param yaw yaw
+   */
   void set(const float x, const float y, const float yaw);
+
+  /**
+   * @brief Get the x-coordinate
+   * @return float x-coordinate
+   */
   float x() const { return x_; }
+
+  /**
+   * @brief Get the y-coordinate
+   * @return float y-coordinate
+   */
   float y() const { return y_; }
+
+  /**
+   * @brief Get the yaw
+   * @return float yaw
+   */
   float yaw() const { return yaw_; }
+
+  /**
+   * @brief Move the pose
+   * @param length length
+   * @param direction direction
+   * @param rotation rotation
+   * @param fw_noise forward noise
+   * @param rot_noise rotation noise
+   */
   void move(float length, float direction, float rotation, const float fw_noise, const float rot_noise);
-  void normalize_angle();
 
 private:
   float x_;    // [m]
